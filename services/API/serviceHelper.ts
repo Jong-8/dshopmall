7dc84a0f5df9fde6c7f9c84ddac3e7fba3ee393f
Jong-8 <anjj1993@gmail.com>
2025-05-21 16:31:53 +0900

    Fix: API 제거

--- services/API/serviceHelper.ts
+++ services/API/serviceHelper.ts
@@ -13,68 +13,68 @@ export const apiHelper = () => {
     baseURL: BASE_URI,
   });
 
-  const _get = async <T>(
-    url: string,
-    config?: AxiosRequestConfig
-  ): Promise<APIResponse<T>> => {
-    try {
-      const response = await client.get<APIResponse<T>>(url, config);
-      return response.data;
-    } catch (error: any) {
-      throw new Error(error.message);
-    }
-  };
+  // const _get = async <T>(
+  //   url: string,
+  //   config?: AxiosRequestConfig
+  // ): Promise<APIResponse<T>> => {
+  //   try {
+  //     const response = await client.get<APIResponse<T>>(url, config);
+  //     return response.data;
+  //   } catch (error: any) {
+  //     throw new Error(error.message);
+  //   }
+  // };
 
-  const _post = async <T>(
-    url: string,
-    data?: any,
-    config?: AxiosRequestConfig
-  ): Promise<APIResponse<T>> => {
-    //console.log(BASE_URI);
-    try {
-      const response = await client.post<APIResponse<T>>(url, data, config);
-      return response.data;
-    } catch (error: any) {
-      throw new Error(error.message);
-    }
-  };
-  const _put = async <T>(
-    url: string,
-    data?: any,
-    config?: AxiosRequestConfig
-  ): Promise<APIResponse<T>> => {
-    try {
-      const response = await client.put<APIResponse<T>>(url, data, config);
-      return response.data;
-    } catch (error: any) {
-      throw new Error(error.message);
-    }
-  };
-  const _delete = async <T>(
-    url: string,
-    config?: AxiosRequestConfig
-  ): Promise<APIResponse<T>> => {
-    try {
-      const response = await client.delete<APIResponse<T>>(url, config);
-      return response.data;
-    } catch (error: any) {
-      throw new Error(error.message);
-    }
-  };
+  // const _post = async <T>(
+  //   url: string,
+  //   data?: any,
+  //   config?: AxiosRequestConfig
+  // ): Promise<APIResponse<T>> => {
+  //   //console.log(BASE_URI);
+  //   try {
+  //     const response = await client.post<APIResponse<T>>(url, data, config);
+  //     return response.data;
+  //   } catch (error: any) {
+  //     throw new Error(error.message);
+  //   }
+  // };
+  // const _put = async <T>(
+  //   url: string,
+  //   data?: any,
+  //   config?: AxiosRequestConfig
+  // ): Promise<APIResponse<T>> => {
+  //   try {
+  //     const response = await client.put<APIResponse<T>>(url, data, config);
+  //     return response.data;
+  //   } catch (error: any) {
+  //     throw new Error(error.message);
+  //   }
+  // };
+  // const _delete = async <T>(
+  //   url: string,
+  //   config?: AxiosRequestConfig
+  // ): Promise<APIResponse<T>> => {
+  //   try {
+  //     const response = await client.delete<APIResponse<T>>(url, config);
+  //     return response.data;
+  //   } catch (error: any) {
+  //     throw new Error(error.message);
+  //   }
+  // };
 
-  const Get = <T>(url: string, config?: AxiosRequestConfig) =>
-    _get<T>(url, config);
-  const Post = <T>(url: string, data?: any, config?: AxiosRequestConfig) =>
-    _post<T>(url, data, config);
-  const Put = <T>(url: string, data?: any, config?: AxiosRequestConfig) =>
-    _put<T>(url, data, config);
-  const Delete = <T>(url: string, config?: AxiosRequestConfig) =>
-    _delete<T>(url, config);
+  // const Get = <T>(url: string, config?: AxiosRequestConfig) =>
+  //   _get<T>(url, config);
+  // const Post = <T>(url: string, data?: any, config?: AxiosRequestConfig) =>
+  //   _post<T>(url, data, config);
+  // const Put = <T>(url: string, data?: any, config?: AxiosRequestConfig) =>
+  //   _put<T>(url, data, config);
+  // const Delete = <T>(url: string, config?: AxiosRequestConfig) =>
+  //   _delete<T>(url, config);
 
-  return {
-    Get,
-    Post,
-    Put,
-    Delete,
-  };
+  // return {
+  //   Get,
+  //   Post,
+  //   Put,
+  //   Delete,
+  // };
 };
