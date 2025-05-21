b753e6e72c369e4a95a8854a55c3db7f85c0de36
Jong-8 <anjj1993@gmail.com>
2025-05-21 16:43:47 +0900

    Fix: 정책 페이지 수정

--- pages/policy.tsx
+++ pages/policy.tsx
@@ -8,16 +8,6 @@ export default function Policy() {
   const [cookies, setCookies, removeCookies] = useCookies(["shopInfo"]);
   const shopInfo = store.shop.useShopInfo();
 
-  const shopInfoApi = async () => {
-    const res = await API.shopInfo.shopInfo();
-    if (res.statusCode === 2000) {
-      shopInfo.setShopInfo(res.result);
-      setCookies("shopInfo", res.result, {
-        path: "/",
-      });
-    } else alert(res.message);
-  };
-
   useEffect(() => {
     if (!shopInfo.shopInfo.company) {
       // 회사정보가 store에 없을 때
@@ -26,7 +16,7 @@ export default function Policy() {
         shopInfo.setShopInfo(cookies.shopInfo);
       } else {
         // 쿠키에 회사정보가 없다면 api로 불러와서 저장
-        shopInfoApi();
+        // ㄴshopInfoApi();
       }
     }
   }, [shopInfo.shopInfo, cookies.shopInfo]);
